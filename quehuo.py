76641c1f30759a3e30fab382cf15cbe852a4a991
solomon <253376634@qq.com>
2012-12-03 15:08:33 +0800

    缺货商品查询

---
+++ quehuo.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+'''
+#=============================================================================
+#     FileName: quehuo.py
+#         Desc: 
+#       Author: solomon
+#        Email: 253376634[at]qq.com
+#     HomePage: 
+#      Version: 0.0.1
+#   LastChange: 2012-12-03 13:47:16
+#      History:
+#=============================================================================
+'''
+from django.template.loader import get_template
+from django.template import Context
+from django.http import HttpResponse
+import confsql,datetime
+from mylog import log
+import datetime
+
+confsql=confsql.Confsql()
+def insult_quehuo(request):
+    """ 缺货查询 """
+    t=get_template('mana1/insult_quehuo.html')
+    sqlstr="select mdcode,mdname,barcode,spcode,spname,prodl_id||'_'||prodl prodl,prozl_id||'_'||prozl prozl,proxl_id||'_'||proxl proxl,braxl_id||'_'||braxl braxl,curqty from dhalldata where assortment='youtu' and curqty<0"
+    result=confsql.runquery(sqlstr)
+    log(result)
+    html=t.render(Context({'result':result}))
+    return HttpResponse(html)
+
